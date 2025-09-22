a71aa65d16c65ade7f46d08f5aae6ba6b36a5bce
Simin <simin.asadzadeh7@gmail.com>
2025-09-22 11:06:01 -0400

    Update q1_create_table.sql

--- q1_create_table.sql
+++ q1_create_table.sql
@@ -1,1 +1,15 @@
+-- Create a new table in the database named WORKOUTLOG with attributes as 
+-- ID (integer, not null), 
+-- EXERCISE (fixed length string up to 20), 
+-- DURATION_MIN (decimal, exercise duration in minutes), 
+-- CALORIES_BURNED (decimal), and 
+-- SESSIONDATE (date).
 
+Answer: 
+CREATE TABLE WORKOUTLOG (
+  ID INTEGER NOT NULL,
+  EXERCISE CHAR(20),
+  DURATION_MIN DECIMAL(5,2),
+  CALORIES_BURNED DECIMAL(6,2),
+  SESSIONDATE DATE
+  );
